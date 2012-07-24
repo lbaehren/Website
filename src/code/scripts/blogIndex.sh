b992fa298f84bd0ab2efd52362b219563be8502b
Lars Baehren <lbaehren@gmail.com>
2012-07-24 10:02:25 +0200

    Recursive processing of blog entries for a complete year; generate index files per month.

--- src/code/scripts/blogIndex.sh
+++ src/code/scripts/blogIndex.sh
@@ -3,7 +3,17 @@
 varExtension=".page"
 varOutfile="index.page"
 
-## === Functions ================================================================
+## ==============================================================================
+##
+##  Function definitions
+##
+## ==============================================================================
+
+##____________________________________________________________________
+##                                                         nameOfMonth
+##
+##  Get the name of the month from the index/number.
+##
 
 nameOfMonth ()
 {
@@ -49,79 +59,153 @@ nameOfMonth ()
   echo $varName
 }
 
-## === Processing ===============================================================
+##____________________________________________________________________
+##                                                    writeIndexHeader
+##
+## Write header of index file; take year as input to generate title.
+##
+## - year - Year for which the index is being created.
+## - menu - Add the page to the menu? If unset, the page will be
+##          excluded from the index in the sidebar.
+##
 
-FILES=`ls *${varExtension} | grep -v index`
-
-##__________________________________________________________
-## Remove existing index file
-
-if [[ -e index.page ]]
-then
-    rm index.page
-fi
-
-##__________________________________________________________
-## Process entry files
-
-for FILE in $FILES
+writeIndexHeader ()
 {
+    _varYear=$1
 
-  ## Remove extension to filename ...
-  varFilename=`echo $FILE | sed s/.page//`
+    if [ -z "$2" ]
+    then
+	_varMenu="false"
+    else
+	_varMenu="true"
+    fi
 
-  ##________________________________________________________
-  ## Extract title of the entry
+    echo "---"
+    echo "title: Blog | $varMonthName ${_varYear}"
+    echo "in_menu: ${_varMenu}"
+    echo "author: Lars Baehren"
+    echo "---"
+    echo ""
+    echo "## Blog archive | ${_varYear} ##"
+}
 
-  varTitle=`grep title: $FILE | grep -v \#\# | sed s/"title: "//`
+##____________________________________________________________________
+##                                                    processDirectory
+##
+##  Process the blog entries within a given directory.
+##
 
-  ##________________________________________________________
-  ## Extract date & time
+processDirectory ()
+{
+    echo "-- Processing directory `pwd` ..."
 
-  varYear=`echo ${varFilename:0:4}`
-  varMonth=`echo ${varFilename:5:2}`
-  varDay=`echo ${varFilename:8:2}`
-  varHour=`echo ${varFilename:11:2}`
-  varMinutes=`echo ${varFilename:14:2}`
+    FILES=`ls *${varExtension} | grep -v index`
+    varIndexFileMonth=`pwd`/$varOutfile
 
-  varMonthName=`nameOfMonth $varMonth`
-  varTime="$varHour:$varMinutes"
+    ## Remove existing index file
+    rm -f $varIndexFileMonth
+    
+    for FILE in $FILES
+    {
+      ## Remove extension to filename ...
+      varFilename=`echo $FILE | sed s/.page//`
 
-  varDateTime=`grep $varTime $FILE | grep $varYear | grep "_" | sed s/"_"//g`
+      ## Extract title of the entry
+      varTitle=`grep title: $FILE | grep -v \#\# | sed s/"title: "//`
 
-  ##________________________________________________________
-  ## Link to generated file
+      ## Extract date & time
+      varMonth=`echo ${varFilename:5:2}`
+      varDay=`echo ${varFilename:8:2}`
+      varHour=`echo ${varFilename:11:2}`
+      varMinutes=`echo ${varFilename:14:2}`
+      varMonthName=`nameOfMonth $varMonth`
+      varTime="$varHour:$varMinutes"
+      varDateTime=`grep $varTime $FILE | grep $varYear | grep "_" | sed s/"_"//g`
 
-  varLink="/blog/$varYear/$varYear-$varMonth/$varFilename.html"
+      ## Link to generated file
+      varLink="/blog/$varYear/$varYear-$varMonth/$varFilename.html"
 
-  ##________________________________________________________
-  ## Summary
+      ## Summary of variables assembled so far
+      echo "--------------"
+      echo "-- Filename    = $varFilename"
+      echo "-- Title       = $varTitle"
+      echo "-- Filename    = $varFilename"
+      echo "-- Timestamp   = $varDateTime"
+      echo "-- Output file = $varIndexFileMonth"
 
-  echo "--------------"
-  echo "Title     = $varTitle"
-  echo "Filename  = $varFilename"
-  echo "Timestamp = $varDateTime"
-
-  ##________________________________________________________
-  ## Generate index file
-
-  if [[ -e index.page ]]
-  then
-      echo "  * [$varTitle]($varLink) | $varDateTime |" >> $varOutfile
-  else 
-      echo "Index file missing - generating header now..."
-      echo "---"                     > $varOutfile
-      echo "title: Blog | $varMonthName $varYear" >> $varOutfile
-      echo "in_menu: false"         >> $varOutfile
-      echo "author: Lars Baehren"   >> $varOutfile
-      echo "---"                    >> $varOutfile
-      echo ""                       >> $varOutfile
-      echo "## {title:} ##"         >> $varOutfile
-      echo ""                       >> $varOutfile
-      echo "  * [$varTitle]($varLink) | $varDateTime |" >> $varOutfile
-  fi
+      ## Generate the index file(s)
+      if [[ -e index.page ]]
+      then
+	  echo "  * [$varTitle]($varLink) | $varDateTime |" >> $varIndexFileMonth
+      else 
+	  echo "Index file missing - generating header now..."
+	  echo "---"                     > $varIndexFileMonth
+	  echo "title: Blog | $varMonthName $varYear" >> $varIndexFileMonth
+	  echo "in_menu: false"         >> $varIndexFileMonth
+	  echo "author: Lars Baehren"   >> $varIndexFileMonth
+	  echo "---"                    >> $varIndexFileMonth
+	  echo ""                       >> $varIndexFileMonth
+	  echo "## {title:} ##"         >> $varIndexFileMonth
+	  echo ""                       >> $varIndexFileMonth
+	  echo "  * [$varTitle]($varLink) | $varDateTime |" >> $varIndexFileMonth
+      fi
+    }
 
 }
+
+## ==============================================================================
+##
+##  Processing
+##
+##  src/pages/blog
+##  |-- YYYY.page
+##  `-- YYYY
+##      |-- YYYY-01
+##      |-- YYYY-02
+##      |
+##      `-- YYYY-12
+##
+## ==============================================================================
+
+##____________________________________________________________________
+## Process command line parameters
+
+if [ -z "$1" ]
+then
+    echo "Missing command line option - year"
+    exit
+else
+    varYear=$1
+fi
+
+##____________________________________________________________________
+## Check if the directory is there
+
+if [ -d $varYear ]
+then
+    echo "Found directory for year $varYear"
+    ## Make the current directory the base directory
+    varBasedir=`pwd`
+else
+    echo "Unable to continue - missing directory $varYear"
+    exit
+fi
+
+cd $varYear
+
+for FILE in `ls`
+{
+    if [ -d $FILE ] 
+    then
+	cd $FILE
+	processDirectory
+	cd $varBasedir/$varYear
+    fi
+}
+
+exit
+
+FILES=`ls *${varExtension} | grep -v index`
 
 ##____________________________________________________________________
 ## Run another pass across the index file for clean-up
