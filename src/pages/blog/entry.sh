0c336932b7a1cf49a8f625f2d85261e41ca9edc7
Lars Baehren <lbaehren@gmail.com>
2014-11-27 19:49:39 +0100

    Add method to publish entry already in preparation, moving it from 'upcoming' to the appropriate location. Put standard operatoins, such as retrieval of timestamps, filenames, etc. into functions to use the same data throughout the script.

--- src/pages/blog/entry.sh
+++ src/pages/blog/entry.sh
@@ -2,12 +2,82 @@
 
 PATH_BASEDIR=`pwd`
 PATH_UPCOMING=upcoming
+EDITOR=atom
 
 # ==============================================================================
 #
 #  Functions
 #
 # ==============================================================================
+
+print_help ()
+{
+    echo "usage: entry.sh [-I|--index] [-P|--publish]"
+    echo ""
+    echo " -H|--help     Print this help and exit."
+    echo " -I|--index    Update index files."
+    echo " -P|--publish  Publish an entry."
+}
+
+#_______________________________________________________________________________
+#                                                                  get_timestamp
+
+get_timestamp ()
+{
+    echo `date +%Y-%m-%dT%H:%M:%S.00Z`
+}
+
+#_______________________________________________________________________________
+#                                                                   get_filename
+
+get_filename ()
+{
+    if [ -z $1 ] ; then
+        varFilename="`date +%Y-%m-%d`_`date +%H-%M`.page"
+    else
+        varFilename=""
+    fi
+
+    echo ${varFilename}
+}
+
+#_______________________________________________________________________________
+#                                                                get_entry_title
+
+get_entry_title ()
+{
+    if [ -z $1 ] ; then
+        echo ""
+    else
+        # check if the file path is valid
+        if [ -f $1 ] ; then
+            varTitle=`grep "title: \"" $1 | sed s/"title: \""// | sed s/\"//`
+            echo ${varTitle}
+        else
+            echo ""
+        fi
+    fi
+}
+
+#_______________________________________________________________________________
+#                                                                 get_timeheader
+
+## Get the (sub-)header with the expanded date and time of the entry
+get_timeheader ()
+{
+    if [ -z $1 ] ; then
+        varTimeheader="`date +%a,\ %d.\ %B\ %Y\ --\ %H:%M`"
+        echo ${varTimeheader}
+    else
+        # check if the file path is valid
+        if [ -f $1 ] ; then
+            varTimeheader=`cat $1 | grep "_" | grep "\-\-" | sed s/_//g`
+            echo ${varTimeheader}
+        else
+            echo ""
+        fi
+    fi
+}
 
 #_______________________________________________________________________________
 #                                                                  create_header
@@ -20,7 +90,7 @@ create_header ()
 {
     varTitle=$1
     varHourMinuteSecond=`date +`
-    varTimestamp=`date +%Y-%m-%dT%H:%M:%S.00Z`
+    varTimestamp=`get_timestamp`
     varTimeheader="`date +%a,\ %d.\ %B\ %Y\ --\ %H:%M`"
 
     echo "---"
@@ -34,6 +104,39 @@ create_header ()
     echo "## {title:} ##"
     echo ""
     echo "_${varTimeheader}_"
+}
+
+#_______________________________________________________________________________
+#                                                         writeHeaderYearArchive
+
+## Create header for contents of year folder
+#
+#  @param outfile -- File to which the output will be written.
+#  @param year    -- Year for which to create the index file.
+#  @param month   -- [optional] Month of the year for which to create the index file.
+#
+create_index_file_header ()
+{
+    varOutfile=${1}
+    varYear=${2}
+
+    if [ -z "$3" ]
+        then
+        varTitle = "${varYear}"
+    else
+        varTitle = "${3} ${varYear}"
+    fi
+
+    echo "-- Writing header for ${varOutfile} ..."
+
+    echo "---"                                        > ${varOutfile}
+    echo "title: ${varTitle}"                        >> ${varOutfile}
+    echo "in_menu: false"                            >> ${varOutfile}
+    echo "author: \"Lars Baehren\""                  >> ${varOutfile}
+    echo "---"                                       >> ${varOutfile}
+    echo ""                                          >> ${varOutfile}
+    echo "## Blog entries | {title:} ##"             >> ${varOutfile}
+    echo ""                                          >> ${varOutfile}
 }
 
 #_______________________________________________________________________________
@@ -53,6 +156,9 @@ create_file ()
 
     # create header for new entry
     create_header ${varTitle} >> ${varOutfile}
+
+    # open the newly created file for further editing
+    open -a ${EDITOR} ${varOutfile}
 
     # update the index file
     create_index_file > ${PATH_BASEDIR}/${PATH_UPCOMING}/index.page
@@ -74,7 +180,7 @@ create_index_file ()
     echo ""
     echo "## {title:} ##"
     echo ""
-    echo "_`date`_"
+    echo "_`get_timeheader`_"
     echo ""
 
     FILES=`ls *.page | grep -v index`
@@ -82,9 +188,9 @@ create_index_file ()
     for FILE in $FILES
     {
         # extract the title of the entry
-        varEntry=`grep "title: \"" ${FILE} | sed s/"title: \""// | sed s/\"//`
+        varEntry=`get_entry_title ${FILE}`
         # extract time header line
-        varTimeheader=`cat ${FILE} | grep "_" | grep "\-\-" | sed s/_//g`
+        varTimeheader=`get_timeheader ${FILE}`
         # set up link pointing to the generated page
         varLink="/blog/${PATH_UPCOMING}/`echo $FILE | sed s/".page"/""/`.html"
 
@@ -93,6 +199,60 @@ create_index_file ()
 
     # change back to the base directory
     cd ${PATH_BASEDIR}
+}
+
+#_______________________________________________________________________________
+#                                                                  list_upcoming
+
+list_upcoming ()
+{
+    cd ${PATH_BASEDIR}
+
+    for FILE in `ls ${PATH_UPCOMING} | grep -v index.page`
+    {
+        varTitle=`get_entry_title ${PATH_UPCOMING}/${FILE}`
+        varTimeheader=`get_timeheader ${PATH_UPCOMING}/${FILE}`
+        echo " - ${FILE} | ${varTitle} | ${varTimeheader}"
+    }
+}
+
+#_______________________________________________________________________________
+#                                                                  publish_entry
+
+publish_entry ()
+{
+    if [ -z $1 ] ; then
+        echo " -> Entries available for publication:"
+        list_upcoming
+    else
+        varTitle=`get_entry_title $1`
+        varFilename=`get_filename`
+        varPath=`date +%Y/%Y-%m`
+        varEntry="${varPath}/${varFilename}"
+        varTimeheaderSource=`get_timeheader $1`
+        varTimeheaderNew=`get_timeheader`
+        varTimestamp=`get_timestamp`
+
+        echo " -> Entry \"${varTitle}\" selected for publication"
+        echo "    - Source file : $1"
+        echo "    - Target file : ${varEntry}"
+        echo "    - Time header : ${varTimeheaderSource} -> ${varTimeheaderNew}"
+
+        if [ -d ${varPath} ] ; then
+            echo "    - Release directory found."
+        else
+            echo "    - Release directory missing - creating it now."
+            mkdir -p ${varPath}
+        fi
+
+        # Parse the input file to replace the time information
+        cat $1 | sed s/"`cat $1 | grep \"date: [0-9]*\"`"/"date: ${varTimestamp}"/ | sed s/"${varTimeheaderSource}"/"${varTimeheaderNew}"/ > ${varEntry}
+        # open the newly created file for further editing
+        open -a ${EDITOR} ${varEntry}
+        # Remove the input file
+        rm -f $1
+
+    fi
 }
 
 # ==============================================================================
@@ -108,18 +268,28 @@ create_index_file ()
 varYearMonth=`date +%Y-%m`
 varYearMonthDay=`date +%Y-%m-%d`
 varHourMinuteSecond=`date +%H:%M:%S`
-varTimestamp=`date +%Y-%m-%dT%H:%M:%S.00Z`
-
-varFilename="`date +%Y-%m-%d`_`date +%H-%M`.page"
-
+varTimestamp=`get_timestamp`
+varFilename=`get_filename`
 
 case $1 in
+    "-H")
+        print_help
+    ;;
+    "--help")
+        print_help
+    ;;
     "-I")
-        echo " - Updating index file for upcoming entries - START"
+        echo "Updating index file for upcoming entries ..."
         create_index_file > ${PATH_BASEDIR}/${PATH_UPCOMING}/index.page
-        echo " - Updating index file for upcoming entries - END"
+    ;;
+    "-P")
+        publish_entry $2
+    ;;
+    "--publish")
+        publish_entry $2
     ;;
     *)
+        echo "Creating file for new blog entry ..."
         create_file ${varFilename} "New_entry" ${varTimestamp}
     ;;
 esac
